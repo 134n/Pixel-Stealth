47f2a1b6c3020da8082349ba9aa37ea5b34de0ba
134n <goruji.ruji@gmail.com>
2024-12-16 06:36:50 +0900

    Add : ステージセレクト画面のシーン遷移機能の追加
    画面の状態インスタンスからシーン状態を選択

---
+++ Assets/Scripts/StageSelect/StageSelect.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using VContainer;
+
+public class StageSelect : MonoBehaviour
+{
+    [SerializeField] Button stage1;
+
+    [SerializeField] Button buck;
+
+    private ScreenChange screenChange;
+
+    [Inject]
+    public void Inject(ScreenChange screenChange)
+    {
+        this.screenChange = screenChange;
+    }
+
+    private void Start()
+    {
+        buck.OnClickAsObservable()
+                .Subscribe(_ => screenChange.ScreenChanged(ScreenStatus.Screen.Title))
+                .AddTo(this);
+
+        stage1.OnClickAsObservable()
+                .Subscribe(_ => screenChange.ScreenChanged(ScreenStatus.Screen.Main))
+                .AddTo(this);
+    }
+}
