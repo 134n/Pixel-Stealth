f69df09ddc8e242e5d3ca79bf2198920ac2bfa3f
134n <goruji.ruji@gmail.com>
2024-12-16 06:15:35 +0900

    Add : 画面の状態に関するクラスの追加

---
+++ Assets/Scripts/ScreenStatus.cs
@@ -0,0 +1,26 @@
+using UniRx;
+using UnityEngine.SceneManagement;
+
+
+//画面の状態に関するクラス
+public class ScreenStatus
+{
+    public enum Screen
+    {
+        Title,
+        StageSelect,
+        Menu,
+        License,
+        Main,
+    }
+}
+
+public sealed class ScreenChange
+{
+    readonly ReactiveProperty<ScreenStatus.Screen> NowScreen = new();
+    public void ScreenChanged(ScreenStatus.Screen nextScreen)
+    {
+        NowScreen.Value = nextScreen;
+        SceneManager.LoadScene(NowScreen.ToString());
+    }
+}
