d2563c24effa97cd0473cc5eef3b7c8bcba63089
134n <goruji.ruji@gmail.com>
2024-12-30 18:30:58 +0900

    Update : VcontainerのLifetimeScopeにアイテム関係のインスタンスを登録

--- Assets/Scripts/Main/Stage1LifetimeScope.cs
+++ Assets/Scripts/Main/Stage1LifetimeScope.cs
@@ -1,3 +1,4 @@
+using Unity.Services.Analytics;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -5,9 +6,17 @@ using VContainer.Unity;
 public class Stage1LifetimeScope : LifetimeScope
 {
     [SerializeField] private EnemyCollider enemyCollider;
+
+    [SerializeField] private PlayerPickItemController playerPickItemController;
+    
     protected override void Configure(IContainerBuilder builder)
     {
         builder.Register<ScreenChange>(Lifetime.Singleton);
         builder.RegisterComponent(enemyCollider);
+
+        builder.Register<ItemKey>(Lifetime.Singleton);
+        builder.Register<ItemMatatabi>(Lifetime.Singleton);
+        builder.Register<PlayerStatus>(Lifetime.Singleton);
+        builder.RegisterComponent(playerPickItemController);
     }
 }
